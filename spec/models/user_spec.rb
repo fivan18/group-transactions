43a2a076df8f412433abc0090be5b8abed70dfed
Ivan <fivanunam@gmail.com>
2020-03-24 12:10:08 -0600

    add test to user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations to test' do
+    it { should validate_presence_of(:username) }
+    it { should validate_length_of(:username).is_at_most(10) }
+  end
+  describe 'associations to test' do
+    #it { should have_many(:transactions) }
+    #it { should have_many(:groups) }
+  end
+end
