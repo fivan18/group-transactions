dc4de71ffc797c0e4b66d157b6b88949653c2db9
Ivan <fivanunam@gmail.com>
2020-03-24 15:32:14 -0600

    add create action for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    store_location
+  end
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user
+      log_in @user
+      redirect_back_or @user
+    else
+      flash.now[:danger] = 'Invalid username'
+      render 'new'
+    end
+  end
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,14 @@
+module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+end
