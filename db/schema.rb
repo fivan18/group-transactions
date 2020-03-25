741335a266630bfdcd0c7b3bbfbc209ef2bf1cc1
Ivan <fivanunam@gmail.com>
2020-03-24 18:43:07 -0600

    add transactions and groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_152154) do
+ActiveRecord::Schema.define(version: 2020_03_25_003829) do
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "author_id"
+    t.string "name", default: "", null: false
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
