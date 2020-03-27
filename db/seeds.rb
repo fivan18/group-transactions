b3251310df2bca71621ef01e8ac802b8fd8a56c8
Ivan <fivanunam@gmail.com>
2020-03-26 22:04:39 -0600

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,20 @@
-7.times do |n|
-  username  = Faker::Name.name[0..9].split(' ').join('').downcase
-  User.create!(username:  username)
-end
+username  = Faker::Name.name[0..9].split(' ').join('').downcase
+User.create!(username:  username)
 
-users = User.all
+user = User.find(1)
 count = 10
-users.each do |user|
+4.times do
   name = Faker::Lorem.sentence
   user.transactions.create!(name: name,
                             amount: count)
   count = count + 10
 end
 
-users.each do |user|
+2.times do
   name = Faker::Company.name
   user.groups.create!(name: name)
 end
 
-transactions = Transaction.order(:created_at).take(2)
-first_group = Group.find(1)
-transactions.each do |transaction|
-  first_group.transactions << transaction
-end
-
-groups = Group.order(:created_at).take(3)
-first_transaction = Transaction.find(1)
-groups.each do |group|
-  first_transaction.groups << group
+user.transactions.take(2).each do |transaction|
+  user.groups.first.transactions << transaction
 end
