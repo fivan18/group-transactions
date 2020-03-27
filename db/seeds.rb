47a1d92b6a2ec6ffed78630cf500a2d28cc8d91e
Ivan <fivanunam@gmail.com>
2020-03-26 19:43:05 -0600

    seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+7.times do |n|
+  username  = Faker::Name.name[0..9].split(' ').join('').downcase
+  User.create!(username:  username)
+end
+
+users = User.all
+count = 10
+users.each do |user|
+  name = Faker::Lorem.sentence
+  user.transactions.create!(name: name,
+                            amount: count)
+  count = count + 10
+end
+
+users.each do |user|
+  name = Faker::Company.name
+  user.groups.create!(name: name)
+end
+
+transactions = Transaction.order(:created_at).take(2)
+first_group = Group.find(1)
+transactions.each do |transaction|
+  first_group.transactions << transaction
+end
+
+groups = Group.order(:created_at).take(3)
+first_transaction = Transaction.find(1)
+groups.each do |group|
+  first_transaction.groups << group
+end
