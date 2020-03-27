c198c79eb2c0abdbc3fff3b42cd7e2001ba57c92
Ivan <fivanunam@gmail.com>
2020-03-27 11:41:59 -0600

    create a transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,26 @@
 class TransactionsController < ApplicationController
+  def new; end
+
+  def create
+    transaction = current_user.transactions.create(transaction_params)
+    if transaction
+      group_id = group_params[:group_id]
+      transaction.groups << Group.find(group_id) unless group_id.empty?
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
   def group_transactions
     @transactions = current_user.group_transactions
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :amount)
+  end
+
+  def group_params
+    params.require(:group).permit(:group_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :sessions, only: [:new, :create]
   resources :users, only: [:show]
+  resources :transactions, only: [:new, :create]
 
   get '/group_transactions', to: 'transactions#group_transactions'
 end
