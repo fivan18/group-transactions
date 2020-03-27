aac405fe1b74359e8ca221ecfda6626650406cc6
Ivan <fivanunam@gmail.com>
2020-03-26 19:42:44 -0600

    set associtions

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :amount_relationships
+  has_many :transactions, through: :amount_relationships, source: :m_transaction
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+  has_many :amount_relationships
+  has_many :groups, through: :amount_relationships
 end
