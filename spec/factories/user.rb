52c96d2b62222303cbdf614fd92466fd7f321174
Ivan <fivanunam@gmail.com>
2020-03-31 22:27:01 -0600

    add some integration test

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,49 @@
 FactoryBot.define do
+  factory :transaction do
+    user
+    name { Faker::Lorem.sentence[7..29] }
+    amount { Faker::Number.number(2) }
+
+    factory :transaction_with_groups do
+      transient do
+        groups_count { 1 }
+      end
+
+      after(:create) do |transaction, evaluator|
+        create_list(:group, evaluator.groups_count)
+      end
+    end
+  end
+
+  factory :group do
+    user
+    name { 'this is a group' }#{ Faker::Company.name[7..29] }
+    avatar { Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/mac.jpeg"), 'image/jpeg') }
+  end
+
   factory :user do
-    username { Faker::Name.name[0..9] }
+    username { Faker::Name.name[0..9].split(' ').join('') }
+    password { 'password' }
+    password_confirmation { 'password' }
+
+    factory :user_with_external_transactions do
+      transient do
+        transactions_count { 1 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:transaction, evaluator.transactions_count, user: user)
+      end
+    end
+
+    factory :user_with_groups do
+      transient do
+        groups_count { 1 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:group, evaluator.groups_count, user: user)
+      end
+    end
   end
 end
---
+++ spec/features/transaction_creation_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Transaction creation', type: :feature, js: true do
+  feature 'different processes to create a transaction' do
+    scenario 'create a transaction no attached to any group' do
+      transaction = create(:transaction)
+      visit external_transactions_path
+      fill_in 'Username', with: transaction.user.username
+      fill_in 'Password', with: 'password'
+      find('input[type="submit"]').click
+
+
+      sleep(1)
+      find('a[href="/transactions/new?selected_group="]').click
+      sleep(1)
+      fill_in 'Name', with: 'Another transaction'
+      sleep(1)
+      fill_in 'Amount', with: '120'
+      sleep(1)
+      find('input[type="submit"]').click
+      sleep(2)
+      expect(page).to have_text("Transaction created!")
+    end
+
+    scenario 'create a transaction attached to group' do
+      transaction = create(:transaction_with_groups)
+      visit group_transactions_path
+      fill_in 'Username', with: transaction.user.username
+      fill_in 'Password', with: 'password'
+      find('input[type="submit"]').click
+
+
+      sleep(1)
+      find('a[href="/transactions/new?selected_group="]').click
+      sleep(1)
+      fill_in 'Name', with: 'Another transaction'
+      sleep(1)
+      fill_in 'Amount', with: '120'
+      sleep(1)
+      find(:select).find(:option, 'this is a group').select_option
+      sleep(1)
+      find('input[type="submit"]').click
+      sleep(2)
+      expect(page).to have_text("Transaction created!")
+    end
+  end
+end
