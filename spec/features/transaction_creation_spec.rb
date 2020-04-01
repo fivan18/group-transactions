e32c1c0a157ecbcfb781d1c1c822bfc89530823c
Ivan <fivanunam@gmail.com>
2020-04-01 12:46:26 -0600

    finish intragration test

--- spec/features/transaction_creation_spec.rb
+++ spec/features/transaction_creation_spec.rb
@@ -43,5 +43,24 @@ RSpec.describe 'Transaction creation', type: :feature, js: true do
       sleep(2)
       expect(page).to have_text("Transaction created!")
     end
+
+    scenario 'create a transaction with a chosen group' do
+      transaction = create(:transaction_with_groups)
+      visit group_path(transaction.groups.first)
+      fill_in 'Username', with: transaction.user.username
+      fill_in 'Password', with: 'password'
+      find('input[type="submit"]').click
+
+      sleep(1)
+      find("a[href=\"/transactions/new?selected_group=#{transaction.groups.first.id}\"]").click
+      sleep(1)
+      fill_in 'Name', with: 'Another transaction'
+      sleep(1)
+      fill_in 'Amount', with: '120'
+      sleep(1)
+      find('input[type="submit"]').click
+      sleep(2)
+      expect(page).to have_text("Transaction created!")
+    end
   end
 end
